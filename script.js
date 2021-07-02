765fa6302ff07ee0bb28f85766e534889ab50aa2
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-07-02 16:52:22 -0600

    concat array for symbols in char codes

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ const form = document.getElementById ('passwordGeneratorForm')
 
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh (48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh (33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
