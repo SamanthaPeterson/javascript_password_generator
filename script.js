45738b32dd9d7aecd677c632d63f42ab924a2eef
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-07-04 20:06:35 -0600

    final

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const characterAmountRange = document.getElementById
 ('characterAmountRange')
 const characterAmountNumber = document.getElementById
 ('characterAmountNumber')
-
-const includeNumbers = get .getElementById('includeNumbers')
-const includeSymbols = get .getElementById('includeSymbols')
+const includeUppercaseElement = document.getElementById
+('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
 const form = document.getElementById ('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
@@ -30,6 +32,7 @@ characterAmountRange.addEventListener('input', syncCharacterAmount)
   const includeNumbers = includeNumbersElement.checked
   const includeSymbols = includeSymbolsElement.checked 
   const password = generatePassword (characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
 })
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
@@ -41,7 +44,7 @@ function generatePassword(characterAmount, includeUppercase, includeNumbers, inc
 
 const passwordCharacters = []
 for (let i = 0; i < characterAmount; i++) {
-  const character = charCodes[Math.floor(Math.random() *
+  const characterCode = charCodes[Math.floor(Math.random() *
     charCodes.length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
@@ -64,43 +67,4 @@ function syncCharacterAmount (e) {
 
 
 
-//  5:26 14:50  https://www.youtube.com/watch?v=iKo9pDKKHnc used this youtube video as refrence for Javavscript code
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//Acceptance Criteria
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
+//  I used this youtube video for refrence to complete the project  https://www.youtube.com/watch?v=iKo9pDKKHnc used this youtube video as refrence for Javavscript code
